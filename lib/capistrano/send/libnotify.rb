cd38483ab730b256798c06fe7fe1cc87940554ee
Guillaume Dott <guillaume+github@dott.fr>
2015-05-22 16:57:01 +0200

    Add `#supported?` method for libnotify and notify-send

--- lib/capistrano/send/libnotify.rb
+++ lib/capistrano/send/libnotify.rb
@@ -1,1 +1,16 @@
+begin
+  require 'libnotify'
+rescue LoadError
+end
+
+module Capistrano
+  module Send
+    module Libnotify
+      def self.supported?
+        defined?(Libnotify) && Libnotify::API.instance_methods.include?(:notify_init)
+      end
+    end
+  end
+end
+
 load File.expand_path('../../tasks/libnotify.cap', __FILE__)
--- lib/capistrano/send/notify-send.rb
+++ lib/capistrano/send/notify-send.rb
@@ -1,1 +1,11 @@
+module Capistrano
+  module Send
+    module NotifySend
+      def self.supported?
+        system('notify-send --version >/dev/null 2>&1') != nil
+      end
+    end
+  end
+end
+
 load File.expand_path('../../tasks/notify-send.cap', __FILE__)
